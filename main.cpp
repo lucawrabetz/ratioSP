f5b998b41e08387f1f8df8c94cb7f4340b36c4be
lucawrabetz <wrabetzluca@gmail.com>
2021-04-15 11:21:00 -0400

    basic io stuff

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <time.h>
+#include <vector>
+
+using std::vector;
+
+vector<int> min_ratio_st(){
+
+}
+
+int main(int argc, char* argv[]) {
+    const clock_t time_0 = clock();
+
+    const std::string datafile = argv[1];
+    const std::string outputfile = argv[2];
+
+    float run_time = float(clock() - time_0) / CLOCKS_PER_SEC;
+
+    return 0;
+}
