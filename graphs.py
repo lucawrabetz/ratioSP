5323f8aed4f35e2d554b05b30edf4becaf8adab7
lucawrabetz <wrabetzluca@gmail.com>
2021-04-27 02:16:20 -0400

    graph generator in python

---
+++ graphs.py
@@ -0,0 +1,66 @@
+import random
+import networkx as nx
+import numpy as np
+
+FILE_NUM = 10
+DENSE_BASE_FILENAME = "_dense_.graph"
+SPARSE_BASE_FILENAME = "_sparse_.graph"
+
+# i is the size of the graph (n)
+# dense graphs are erdos renyi with p = 0.8
+# sparse graphs are erdos renyi with p = 0.5
+
+DENSE_FILE_NAMES = [str(i+4) + DENSE_BASE_FILENAME for i in range(FILE_NUM)]
+SPARSE_FILE_NAMES = [str(i+4) + SPARSE_BASE_FILENAME for i in range(FILE_NUM)]
+
+# values for average and standard deviation for normal distribution for costs
+mu_c = 250
+mu_tau = 400
+sigma = 50
+
+def write_graph(filename, N, dense=True):
+    connected = False
+    if dense:
+        prob = 0.8
+    else:
+        prob = 0.5
+
+    while connected==False:
+        node_node_matrix = [[0 for j in range(N)] for i in range(N)]
+        exist_list = list(np.random.binomial(n=1, p=prob, size=N**2))
+        cost_list = list(np.random.normal(mu_c, sigma, size=N**2))
+        tau_list = list(np.random.normal(mu_tau, sigma, size=N**2))
+        print(exist_list)
+        print(cost_list)
+        print(tau_list)
+        exists = 0
+        counter = 0
+        m = 0
+        G = nx.Graph()
+
+        for u in range(N):
+            for v in range(u+1, N):
+                exists = exist_list[counter]
+                if exists:
+                    G.add_edge(u, v, cost=int(cost_list[counter]), tau=int(tau_list[counter]))
+                    m += 1
+                counter += 1
+
+
+        # ensure graph is connected
+        if nx.is_connected(G):
+            connected = True
+        else: continue
+
+        # write edge list to file
+        nx.write_edgelist(G, filename, data=['cost', 'tau'])
+
+        # prepend n and m to first line
+        with open(filename, 'r') as original: data = original.read()
+        with open(filename, 'w') as new: new.write(str(N) + " " + str(m) + "\n" + data)
+
+    return
+
+for i in range(FILE_NUM):
+    write_graph(DENSE_FILE_NAMES[i], i+4)
+    write_graph(SPARSE_FILE_NAMES[i], i+4, False)
--- main.cpp
+++ main.cpp
@@ -8,10 +8,14 @@
 #include <queue>
 #include <string>
 #include <cmath>
+#include <random>
 
 using std::set;
 using std::vector;
 using std::priority_queue;
+using std::random_device;
+using std::mt19937;
+using std::uniform_real_distribution;
 using std::cout;
 using std::cin;
 using std::endl;
@@ -55,11 +59,14 @@ public:
         H = _H;
     }
     
-    float getH() const {
-        return H;
+    int get_i() const { return i; }
+    int get_j() const { return j; }
+    int get_e() const { return edge_index; }
+    float getH() const { 
+        return H; 
     }
 
-    void update_Edge(float _new_H) {
+    void update_H(float _new_H) {
         H = _new_H;
     }
 };
@@ -109,99 +116,73 @@ public:
 };
 
 
-//void mst_kruskal(int n, int m, float C, const vector<int>& neighbors, const vector<int>& indexes, const vector<int>& c, MST* t_star) {
-//    /*
-//     * Subroutine for finding MST(k) - kruskals
-//     * 
-//     * Receive current graph topology and costs 
-//     * Receive MST address to update
-//     * Initialize --> heap of edges and costs, and the bool vector of whos in the tree already (nodes)  
-//     * While T^{*}.edges is not has size less than n-1, pop next edge, check for cycle, add edge 
-//     * Updates the MST struct T^{*} without returning anything 
-//     */
-//    
-//    // pre initialization
-//    // t_star is the MST (indexes of edges as per lists neighbors, h, c, tau)
-//    // t_star has attributes edges and obj_val
-//    // other values - , 
-//    t_star->clear(); 
-//    
-//        
-//    // initialize heap of edges 
-//    priority_queue <Edge, vector<Edge>, EdgeComparator> heap;
-//    //vector<int> pred; // vector of predecessors
-//    vector<bool> S; // who is already in the tree? (nodes)
-//    int temp_index; // to hold the vertex index
-//    float temp_dval; // to hold the distance label
-//    int start_index; // to hold the index to start looping through in edge list
-//    int stop_index; // to hold the index to stop looping through in edge list
-//
-//    int min_edge_index; // for the index of the minimum edge to be added to t_star
-//    float min_edge_cost; // for the cost of the minimum edge to be added to t_star
-//
-//    for (int i = 0; i < n; ++i) {
-//        S.push_back(false);
-//    }       
-//
-//    for (int e = 0; e < m; ++e) {
-//    }
-//
-//    start_index = indexes[0]; 
-//    stop_index = indexes[1];
-//
-//    for (int j = start_index; j < stop_index; ++j) {
-//        // push neighbors of node 0 to heap with distance label c_0j and set their pred = 0
-//        heap.push(Vertex(neighbors[j], c[j])); // add node 0 to heap 
-//        pred[neighbors[j]] = 0;
-//    }    
-//
-//    // set S[0] to true - consider node 0 in the tree 
-//    S[0] = true; 
-//
-//    /* use this loop to test that the heap is initializing correctly 
-//    float d_val;
-//    while (!heap.empty()) {
-//        d_val = heap.top().d_i;
-//        cout << d_val << endl;
-//        heap.pop();
-//    }
-//    */
-//
-//    // main loop
-//    // invariant - while T^{*} is not of size n-1 
-//    
-//    while (t_star->edges.size() < n-1) {
-//        // get the vertex with next lowest d_{i} from the heap, and delete from the heap
-//        temp_index = heap.top().i; 
-//        temp_dval = heap.top().d_i;
-//        heap.pop();
-//
-//        start_index = indexes[temp_index];
-//        stop_index = indexes[temp_index+1];
-//
-//        for (int e = start_index; e < stop_index; ++e) {
-//            if (!S[neighbors[e]]) {
-//                // if the neighbor is not already in S, push neighbor of node to heap with distance label c and set their pred
-//                heap.push(Vertex(neighbors[e], c[e])); // add node 0 to heap 
-//                pred[neighbors[e]] = temp_index;
-//            }
-//            if (neighbors[e] == pred[temp_index]) {
-//                // set the index of the edge we are adding to e - this way we can keep track of it t_star by edge index and not as a tuple
-//                min_edge_index = e;
-//                min_edge_cost = c[e];
-//            }
-//        }
-//        
-//        t_star->add_edge(min_edge_index);
-//        t_star->set_objective(t_star->obj_val + min_edge_cost);
-//        // update the heap d_i values of each neighbor - !!!! we will re-add these neighbors 
-//        // since priority_queue has no decrease_key implementation, so we will have duplicates
-//        // but the ones with d = C+1 will not get popped so its fine
-//        
-//    }
-//    
-//    return;
-//}
+void mst_kruskal(int n, int m, float C, const vector<Edge>& edges, MST* t_star) {
+    /*
+     * Subroutine for finding MST(k) - kruskals
+     * 
+     * Receive current graph topology and costs - edge indexes are indexed 0-m
+     * Receive MST address to update
+     * Initialize --> heap of edges and costs, and the bool vector of whos in the tree already (nodes)  
+     * While T^{*}.edges is not has size less than n-1, pop next edge, check for cycle, add edge 
+     * Updates the MST struct T^{*} without returning anything 
+     */
+    
+    // pre initialization
+    // t_star is the MST (indexes of edges as per lists neighbors, h, c, tau)
+    // t_star has attributes edges and obj_val
+    t_star->clear(); 
+        
+    // initialize heap of edges 
+    priority_queue <Edge, vector<Edge>, EdgeComparator> heap;
+    for (int e = 0; e < edges.size(); ++e) {
+        heap.push(edges[e]);
+    }
+    vector<bool> S; // who is already in the tree? (nodes)
+    int temp_i; // to hold the first index
+    int temp_j; // to hold the second index
+    float temp_H; // to hold the cost 
+    float temp_e; // to hold the edge index 
+    int start_index; // to hold the index to start looping through in edge list
+    int stop_index; // to hold the index to stop looping through in edge list
+
+    // initialize no nodes in the tree
+    for (int i = 0; i < n; ++i) {
+        S.push_back(false);
+    }       
+
+    // use this loop to test that the heap is initializing correctly 
+    // while (!heap.empty()) {
+    //     temp_H = heap.top().getH();
+    //     cout << temp_H << endl;
+    //     heap.pop();
+    // }
+    
+
+    // main loop
+    // invariant - while T^{*} is not of size n-1 
+    
+    while (t_star->edges.size() < n-1) {
+        // get the vertex with next lowest d_{i} from the heap, and delete from the heap
+        temp_i = heap.top().get_i(); 
+        temp_j = heap.top().get_j();
+        temp_H = heap.top().getH();
+        temp_e = heap.top().get_e();
+        heap.pop();
+
+        if (!S[temp_i] || !S[temp_j]) {
+            // if at least one of the endpoints of the node is not yet in the tree,
+            // it does not create a cycle in the graph, so we add it
+            // update S to ensure both endpoints are now marked as 'true'
+            t_star->add_edge(temp_e);
+            t_star->set_objective(t_star->obj_val + temp_H);
+            S[temp_i] = true;
+            S[temp_j] = true;
+        }
+        else { continue; } 
+    }
+    
+    return;
+}
 
 
 void mst_prims(int n, int m, float C, const vector<int>& neighbors, const vector<int>& indexes, vector<float> c, MST* t_star) {
@@ -510,7 +491,7 @@ int main(int argc, char* argv[]) {
     // const std::string datafile = argv[1]; 
     // const std::string outputfile = argv[2]; 
     // to hard code the datafiles 
-    const std::string datafile = "example.graph";
+    const std::string datafile = "complete6.graph";
     const std::string outputfile = "output.txt";
     
     // initialize file objects
@@ -538,6 +519,7 @@ int main(int argc, char* argv[]) {
     vector<int> indexes;
     vector<int> raw_indexes;
     vector<float> h;
+    vector<Edge> edges;
     float C; 
     std::string line;
     
@@ -563,6 +545,7 @@ int main(int argc, char* argv[]) {
                     c_raw.push_back(edge_c);
                     tau_raw.push_back(edge_tau);
                     raw_indexes.push_back(counter-2);
+                    edges.push_back(Edge(counter-2, u, v, edge_c)); // initially initialize vertex with cost c - every iteration that calls kruskals will also update those costs
                     if (edge_c > max_c) {
                         max_c = edge_c;
                     }
@@ -635,19 +618,44 @@ int main(int argc, char* argv[]) {
     const vector<int> final_tau_raw = tau_raw;
     const vector<int> final_raw_indexes = raw_indexes;
     const vector<int> final_indexes = indexes;
+    const vector<Edge> final_edges = edges;
+    const float k = 1; 
     MST* t_star = new MST();
+    MST* t_star_kruskals = new MST();
     
 
-    float obj_val = min_ratio_st(n, m, C, final_c_raw, final_tau_raw, final_raw_indexes, final_neighbors, final_c,  final_tau, final_indexes, t_star);
-    cout<<endl;
-    cout<<"Terminated MRST with obj: "<<obj_val<<endl;
+    // float obj_val = min_ratio_st(n, m, C, final_c_raw, final_tau_raw, final_raw_indexes, final_neighbors, final_c,  final_tau, final_indexes, t_star);
+    // cout<<endl;
+    // cout<<"Terminated MRST with obj: "<<obj_val<<endl;
     // mst_prims(n, m, C, final_neighbors, final_indexes, final_c, t_star);
-    //cout<<"prims obj: "<<t_star->obj_val<<endl;
-    //for (int i = 0; i < n-1; ++i) {
-    //    cout<<t_star->edges[i]<<endl;
-    //}
+    // cout<<"prims obj: "<<t_star->obj_val<<endl;
+    // mst_kruskal(n, m, C, final_edges, t_star);
 
-    float run_time = float(clock() - time_0) / CLOCKS_PER_SEC;
+    random_device rd;
+    mt19937 e2(rd());
+    uniform_real_distribution<> dist(0, 1000);
+    // stress test between mst_kruskal and mst_prims
+    bool different = false; 
+    while (!different) {
+        
+        // kruskals     
+        
+         
+        // prims
+        const vector<int> final_c = c;
 
-    return 0;
+        // kruskals
+        const vector<Edge> final_edges = edges;
+
+        mst_prims(n, m, C, final_neighbors, final_indexes, final_c, t_star);
+        mst_kruskal(n, m, C, final_edges, t_star);
+    }
+    // for (int i = 0; i < n-1; ++i) {
+    //     cout<<t_star->edges[i]<<endl;
+    // }
+    // cout<<t_star->obj_val<<endl;
+
+    // float run_time = float(clock() - time_0) / CLOCKS_PER_SEC;
+
+    // return 0;
 }
